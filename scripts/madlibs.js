a997b3cf509bc7b98e4cf9033109553b6fcaee8b
Matthew Wainwright <mwwainwright1014@gmail.com>
2026-08-13 07:52:42 -0500

    random closing

--- scripts/madlibs.js
+++ scripts/madlibs.js
@@ -1,11 +1,25 @@
 let name = prompt("A name");
 let adjective = prompt("An adjective");
 let noun = prompt("A noun");
+let closings = [
+    "You are a legend.",
+    "Stay golden.",
+    "Signs point to awesome.",
+    "Keep being weird.",
+    "Chaos suits you."
+];
+
+function randomNumber(a, b) {
+    return Math.floor(Math.random() * (b - a + 1)) + a;
+}
 
 function makeCard() {
 console.log("Dear " + name + ",");
 console.log("You are as " + adjective + " as a " + noun + ".");
 console.log("Happy birthday from the generator.");
+
+let index = randomNumber(0, closings.length - 1);
+console.log(closings[index]);
 }
 
-setInterval(makeCard, 2000);
+makeCard();
